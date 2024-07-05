f4913897d0f3efe7b77c88394e68dc137f534436
Rikimo23 <162639270+Rikimo23@users.noreply.github.com>
2024-07-05 14:26:28 -0400

    first commit

--- src/javastringtoken.java
+++ src/javastringtoken.java
@@ -20,7 +20,7 @@ public class javastringtoken {
 
         String[] words = s.split("[^a-zA-Z]+");
 
-       
+
         System.out.println(words.length);
         for (String word : words) {
             System.out.println(word);
